3a5b1502fd90b3849475d9739b41dcdbe014971b
Ritesh Kumar shukla <riteshshukla2381@gmail.com>
2021-10-01 12:54:24 +0530

    Input Outer Box Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css';
+import InputBox from './components/inputBox';
 
 function App() {
   return (
-    <div>
-      <h1>Test</h1>
-    </div>
+
+      <InputBox></InputBox>
+    
   );
 }
 
