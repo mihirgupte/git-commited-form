eb1e986e8431e1f2b22e2133a1acd0c826d7115d
unknown <mihir.a.gupte@gmail.com>
2021-10-02 20:00:15 +0530

    added text input fields

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import './App.css';
+import './components/inputBox/index.css'
+import './components/button/index.css'
+import './components/textInput/index.css'
 import InputBox from './components/inputBox';
+import InputField from './components/textInput';
 import Button from './components/button';
+import React from 'react';
 
 function App() {
+  const [val1,setVal1] = React.useState("")
+  const [val2,setVal2] = React.useState("")
+  const [val3,setVal3] = React.useState("")
   return (
 
       <InputBox>
+      <InputField type="email" placeholder="Email" value={val3} onChange={e => setVal3(e.target.value)}></InputField>
+      <InputField type="password" placeholder="Password" value={val2} onChange={e => setVal2(e.target.value)}></InputField>
+      <InputField type="password" placeholder="Confirm Password" value={val1} onChange={e => setVal1(e.target.value)}></InputField>
       <Button >Previous</Button>
       <Button>Next</Button>
       </InputBox>
--- src/components/textInput/index.jsx
+++ src/components/textInput/index.jsx
@@ -2,7 +2,7 @@ import './index.css';
 export default function InputField({type, placeholder, value, onChange}){
     return(
         <>
-            <input type={type} placeholder={placeholder} value={value} onChange={(e)=>onChange(e.target.value)}></input>
+            <input type={type} placeholder={placeholder} value={value} onChange={onChange}></input>
         </>
     )
 }
